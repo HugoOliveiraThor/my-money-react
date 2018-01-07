a7b19dce9b052c7a2d5d685d0a6dd5d916f53eae
HugoOliveiraThor <hugooliveirahenrique@gmail.com>
2018-01-07 17:00:27 -0200

    Criação das dependencias de js e css do template ADMIN-lte Criacao do sidebar Criacao do menu Criacao do header Atualização dos componentes app.jsx do sidebar e menu Configuração de ajustes das fontes de leitura do webpack.config Criação do custom.css para adição do css .

---
+++ client/src/main/app.jsx
@@ -0,0 +1,12 @@
+import '../commom/template/dependencies'
+import React from 'react'
+
+import Header from '../commom/template/header'
+import SideBar from '../commom/template/sideBar'
+
+export default props => (
+        <div className='wrapper'>
+        <Header /> 
+        <SideBar />
+        </div>
+)
