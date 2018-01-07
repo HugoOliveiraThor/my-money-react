7a5d35e20b627c77fd03781cdedefbb1cc5aa137
HugoOliveiraThor <hugooliveirahenrique@gmail.com>
2018-01-07 19:56:25 -0200

    Criando o footer Ajustando o css do footer que está com defeito no main-footer

---
+++ client/src/commom/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright Hugo Oliveira &copy; 2017
+            <a href='http://www.hugooliveira.com' target='_blank'>Hugo Oliveira</a> 
+        </strong>
+    </footer>
+)
