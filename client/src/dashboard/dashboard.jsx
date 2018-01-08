44a59845778eb7771a63deab9f2c27001ec73326
Hugo <hugooliveirahenrique@gmail.com>
2018-01-08 15:16:39 -0200

    Adicionando o row Dentro do valueBox para alinhamento

--- client/src/dashboard/dashboard.jsx
+++ client/src/dashboard/dashboard.jsx
@@ -3,6 +3,7 @@ import React, { Component } from 'react'
 import ContentHeader from '../commom/template/contentHeader'
 import Content from '../commom/template/content'
 import ValueBox from '../commom/widget/valueBox'
+import Row from '../commom/layout/row'
 
 class Dashboard extends Component {
     render() {
@@ -10,10 +11,12 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total de créditos' />
-                    <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 20' text='Total de debitos'/>
-                    <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Valor consolidado' /> 
-                </Content>    
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total de créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 20' text='Total de debitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Valor consolidado' />
+                    </Row>
+                </Content>
             </div>
         )
     }
