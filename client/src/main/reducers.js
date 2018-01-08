e0eee46d3bd988a97d1e4cc21c9ab67b1adb026a
Hugo <hugooliveirahenrique@gmail.com>
2018-01-08 18:36:14 -0200

    Criando o tabActions e TabReducer para a acao de trocar a aba

--- client/src/main/reducers.js
+++ client/src/main/reducers.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import DashboardReducer from '../dashboard/dashBoardReducer'
+import TabReducer from '../commom/tab/tabReducer'
 
 
 const rootReducer = combineReducers({
-    dashboard: DashboardReducer
+    dashboard: DashboardReducer,
+    tabs: TabReducer
 })
 
 export default rootReducer
