0717e981161d2533ba6ac762f7a23743b93cefa9
Hugo <hugooliveirahenrique@gmail.com>
2018-01-08 17:03:14 -0200

    Criando uma versão do dashboard sem redux

---
+++ client/src/dashboardSemRedux/dashboardSemRedux.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+import ContentHeader from '../commom/template/contentHeader'
+import Content from '../commom/template/content'
+import ValueBox from '../commom/widget/valueBox'
+import Row from '../commom/layout/row'
+const BASE_URL = 'http://localhost:3003/api'
+
+export default class Dashboard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { credit:0, debt:0 }
+    }
+    componentWillMount() {
+        axios.get(`${BASE_URL}/billingCycles/summary`).then(resp => this.setState(resp.data))
+    }
+    render() {
+        const {credit , debt} = this.state
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 2.0 sem Redux' />
+                <Content>
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value={`R$ ${credit}`} text='Total de créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value={`R$ ${debt}`} text='Total de debitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money' value={`R$ ${credit-debt}`} text='Valor consolidado' />
+                    </Row>
+                </Content>
+            </div>
+        )
+    }
+}
+
+
--- client/src/main/routes.jsx
+++ client/src/main/routes.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import {Router , Route , Redirect , hashHistory } from 'react-router'
 
 import Dashboard from  '../dashboard/dashboard'
+import Dashboard2 from '../dashboardSemRedux/dashboardSemRedux'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
     <Router history={hashHistory}>
-        <Route path='/' component={Dashboard} />
+        <Route path='/' component={Dashboard2} />
         <Route path='/billingCycles' component={BillingCycle}  />
         <Redirect from= '*' to='/' />   
     
