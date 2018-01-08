815649c34e4724ac13c72a1ab0c574d9d22897ae
Hugo <hugooliveirahenrique@gmail.com>
2018-01-08 17:51:13 -0200

    Ajustando o componente billingCycle para comecar a receber os imports

--- client/src/billingCycle/billingCycle.jsx
+++ client/src/billingCycle/billingCycle.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-    <div>
-        <h1> Ciclo de pagamento </h1>
-    </div>
-)
+import ContentHeader from '../commom/template/contentHeader'
+import Content from '../commom/template/content'
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de pagamento' small='Cadastro'/>
+                <Content>
+                    Ciclos de pagamento
+                </Content>
+
+            </div>
+
+        )
+    }
+}
+
+export default BillingCycle
