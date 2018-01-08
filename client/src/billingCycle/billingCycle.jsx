05b19805472191e623e977992fa8b0f2ddc77bcd
Hugo <hugooliveirahenrique@gmail.com>
2018-01-08 18:21:30 -0200

    Juntando os compoenentes para criar as abas do billingCycle

--- client/src/billingCycle/billingCycle.jsx
+++ client/src/billingCycle/billingCycle.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../commom/template/contentHeader'
 import Content from '../commom/template/content'
+import Tabs from '../commom/tab/tabs'
+import TabsHeader from '../commom/tab/tabsHeader'
+import TabsContent from '../commom/tab/tabsContent'
+import TabHeader from '../commom/tab/tabHeader'
 
 class BillingCycle extends Component {
     render() {
@@ -9,7 +13,18 @@ class BillingCycle extends Component {
             <div>
                 <ContentHeader title='Ciclos de pagamento' small='Cadastro'/>
                 <Content>
-                    Ciclos de pagamento
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList'/>
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate'/>
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate'/>
+                            <TabHeader label='Excluir' icon='trash-o' target='tabDelete'/>
+                        </TabsHeader>
+                        <TabsContent>
+
+                        </TabsContent>
+                    </Tabs>
+                   
                 </Content>
 
             </div>
