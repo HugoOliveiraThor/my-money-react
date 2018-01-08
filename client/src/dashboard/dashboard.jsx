401df58a91b7dd18e10200061b9e28b5d94da759
Hugo <hugooliveirahenrique@gmail.com>
2018-01-08 15:12:51 -0200

    Adicionando o valueBox dentro do dashboard

--- client/src/dashboard/dashboard.jsx
+++ client/src/dashboard/dashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../commom/template/contentHeader'
 import Content from '../commom/template/content'
+import ValueBox from '../commom/widget/valueBox'
 
 class Dashboard extends Component {
     render() {
@@ -9,7 +10,9 @@ class Dashboard extends Component {
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
                 <Content>
-                    Dashboard
+                    <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total de créditos' />
+                    <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 20' text='Total de debitos'/>
+                    <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Valor consolidado' /> 
                 </Content>    
             </div>
         )
