cb647f311880d2513e3d49b6a7bc759fdc505ce2
Hugo <hugooliveirahenrique@gmail.com>
2018-01-10 18:03:51 -0200

    Adicionando dinamicamente as abas

--- client/src/billingCycle/billingCycle.jsx
+++ client/src/billingCycle/billingCycle.jsx
@@ -17,6 +17,7 @@ class BillingCycle extends Component {
 
     componentWillMount() {
         this.props.selectTab('tabList')
+        this.props.showTabs('tabList','tabCreate')
     }
     render() {
         return (
