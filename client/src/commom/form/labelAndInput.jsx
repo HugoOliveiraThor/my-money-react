e910bdd42187d4f4b034d315e0851958cee83859
Hugo <hugooliveirahenrique@gmail.com>
2018-01-15 15:42:59 -0200

    BillingCycle adicao dos fields .

--- client/src/commom/form/labelAndInput.jsx
+++ client/src/commom/form/labelAndInput.jsx
@@ -2,5 +2,10 @@ import React from 'react'
 import Grid from '../layout/grid'
 
 export default props => (
-
+    <Grid cols={props.cols}>
+        <label htmlFor={props.name}>{props.label}</label>
+        <input {...props.input} className='form-control'
+            placeholder={props.placeholder}
+            readOnly={props.readOnly} type={props.type}/>
+    </Grid>
 )
