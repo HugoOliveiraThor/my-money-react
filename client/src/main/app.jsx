f194e6d494178d174a057169e473c150959c83c6
Hugo <hugooliveirahenrique@gmail.com>
2018-01-15 13:00:57 -0200

    Criando o controller de  messages.jsx app.jsx reducers.js

--- client/src/main/app.jsx
+++ client/src/main/app.jsx
@@ -5,6 +5,7 @@ import Header from '../commom/template/header'
 import SideBar from '../commom/template/sideBar'
 import Footer from '../commom/template/footer'
 import Routes from './routes'
+import Messages from '../commom/msg/messages'
 
 export default props => (
         <div className='wrapper'>
@@ -14,5 +15,6 @@ export default props => (
                 <Routes />
         </div> 
         <Footer />
+        <Messages />
         </div>
 )
