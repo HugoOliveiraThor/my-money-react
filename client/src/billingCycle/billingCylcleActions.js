b393af94bfa5c93b9705dbe2ac5809de4ac9f99b
Hugo <hugooliveirahenrique@gmail.com>
2018-01-15 14:10:55 -0200

    Adicionando o dispatch para efetuar varias operacoes .

--- client/src/billingCycle/billingCylcleActions.js
+++ client/src/billingCycle/billingCylcleActions.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import {reset as resetForm } from 'redux-form'
+import { showTabs, selectTab } from '../commom/tab/tabActions'
+
+
 const BASE_URL = 'http://localhost:3003/api'
 
 
@@ -10,10 +14,22 @@ export function getList() {
         payload: request 
     }
 }
-
+//Somente é possivel fazer um dispatch dessa maneira por causa do REDUX MULTI 
 export function create(values) {
-    axios.post(`${BASE_URL}/billingCycles`, values)
-    return {
-        type: 'TEMP'
+    return dispatch => {
+        axios.post(`${BASE_URL}/billingCycles`, values).then(resp => {
+            toastr.success('Sucesso','Operação realizada com sucesso!')
+            dispatch([
+                resetForm('billingCycleForm'),
+                getList(),
+                selectTab('tabList'),
+                showTabs('tabList','tabCreate')
+            ])
+        }).catch(e => {
+            e.response.data.errors.forEach(error => toastr.error('Erro', error))
+        })
+        return {
+            type: 'TEMP'
+        }
     }
 }
