13d0b2d9778e52f14dda7b0171b487588af986c4
Hugo <hugooliveirahenrique@gmail.com>
2018-01-18 21:06:07 -0200

    Finalizacao cadastro basico

--- client/src/billingCycle/billingCycleForm.jsx
+++ client/src/billingCycle/billingCycleForm.jsx
@@ -30,7 +30,7 @@ class BillingCycleForm extends Component {
                         placeholder='Informe o ano'/>
                 </div>
                 <div className='box-footer'>
-                    <button type='submit' className='btn btn-primary'>Submit</button>
+                    <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
                     <button type='button' className='btn btn-default' onClick={this.props.init}>Cancelar</button>
                 </div>
             
